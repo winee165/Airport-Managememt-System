0e49fa83079ba8a3de2917325e12d656e7a34a55
Mudit <mudit.jindal14224@gmail.com>
2022-04-17 19:51:20 +0530

    Connected to css and js

--- Airport_Management_System/Airport_Management_System/settings.py
+++ Airport_Management_System/Airport_Management_System/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,14 +117,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Addded Manually
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# Addded ManuallySTATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
